2877e9a9d31591314277ce5d67a84b208e36fd22
firasboudhraa <fboudhraa2@gmail.com>
2025-09-21 12:05:49 +0100

    updating the jenkins file for the test

--- tpfoyer/src/test/java/tn/esprit/tpfoyer/TpfoyerApplicationTests.java
+++ tpfoyer/src/test/java/tn/esprit/tpfoyer/TpfoyerApplicationTests.java
@@ -1,9 +1,11 @@
 package tn.esprit.tpfoyer;
 
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AllArgsConstructor
 class TpfoyerApplicationTests {
 
     @Test
